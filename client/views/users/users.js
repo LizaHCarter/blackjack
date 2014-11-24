b690b2134a5697b0249560029549b0b7cee1592c
Liza Carter <lizahcarter@gmail.com>
2014-11-24 13:52:55 -0600

    avatar link only shows on register page

--- client/views/users/users.js
+++ client/views/users/users.js
@@ -5,6 +5,7 @@
     .controller('UsersCtrl', ['$rootScope', '$scope', '$state', 'User', function($rootScope, $scope, $state, User){
       $scope.user = {};
       $scope.mode = $state.current.name;
+      $scope.showAvatar = $scope.mode === 'login';
 
       $scope.submit = function(){
         if($scope.mode === 'register'){
